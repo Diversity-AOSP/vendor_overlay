056a0c983e7c71fa9d2313091b1d6d57a91f3b4a
rdx420 <padraramesh420@gmail.com>
2023-05-27 23:12:57 +0200

    overlays: Add more Brightness Slider from Iconify [3/3]
    Credit - https://github.com/Mahmud0808/Iconify
    
    Signed-off-by: helliscloser <mrumais@gmail.com>

--- overlays.mk
+++ overlays.mk
@@ -160,7 +160,21 @@ PRODUCT_PACKAGES += \
 # Brightness slider styles
 PRODUCT_PACKAGES += \
     BrightnessSliderFilledOverlay \
-    BrightnessSliderThinOverlay
+    BrightnessSliderThinOverlay \
+    BrightnessSliderCyberpunkOverlay \
+    BrightnessSliderGradiantOverlay \
+    BrightnessSliderNeumorphOverlay \
+    BrightnessSliderOutlineOverlay \
+    BrightnessSliderShadedOverlay \
+    BrightnessSliderThinOutlineOverlay \
+    BrightnessSliderTranslucentOverlay \
+    BrightnessSliderLeafyOutlineOverlay \
+    BrightnessSliderMinimalThumbOverlay \
+    BrightnessSliderOldSchoolThumbOverlay \
+    BrightnessSliderRoundedClipOverlay \
+    BrightnessSliderThumbSliderOverlay \
+    BrightnessSliderLineOverlay \
+    BrightnessSliderLightyOverlay 
 
 # QS Panel
 PRODUCT_PACKAGES += \
